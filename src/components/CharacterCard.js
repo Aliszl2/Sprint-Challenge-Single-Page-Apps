e95a2be2368c7f67ce6f74233b83ac76b720d430
Alison Law <alisparklaw@gmail.com>
2020-01-31 08:21:06 +0000

    Charachter page basic card styling

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,19 +3,7 @@ import CharacterList from "./CharacterList";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-// styling
-const StyledDiv = styled.div`
-  margin: 20px auto;
-  width: 60%;
-  border: grey 2px solid;
-  height: auto;
-  border-radius: 4px;
-  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
-  overflow: hidden;
-  &:hover {
-    background-color: skyblue;
-  }
-`;
+
 
 const CharacterCard = props => {
   return (
@@ -31,4 +19,22 @@ const CharacterCard = props => {
 
 export default CharacterCard;
 
-
+// styling
+const StyledDiv = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+  margin: 20px auto;
+  width: 60%;
+  border: grey 2px solid;
+  height: auto;
+  border-radius: 4px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  overflow: hidden;
+  &:hover {
+    background-color: skyblue;
+  }
+  img{
+    width:60%;
+  }
+`;
